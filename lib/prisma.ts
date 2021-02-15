1768085711dd42004c9fbc114870d1d4c392e5fc
Nikolas <nikolas.burk@gmail.com>
2021-02-15 11:39:27 +0100

    Update prisma.ts

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,4 +1,15 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client';
 
-const prisma = new PrismaClient();
+let prisma: PrismaClient;
+
+// https://pris.ly/help
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient();
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
+}
+
 export default prisma;
